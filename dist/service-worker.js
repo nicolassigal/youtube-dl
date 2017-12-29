fffab9ae1835b801a76b630b7848e7def33775ce
nicolassigal <nicolas.sigal@fisglobal.com>
2017-12-29 16:27:19 -0300

    asd

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/index.html","88ad17614d530a36ebe15a916e7d61a7"],["/inline.49af03f940af0fd47078.bundle.js","91574ef453d27fb8b205b4defd16ed37"],["/main.95e9f4bf97204f08e84e.bundle.js","7361e874b6671838983969d8fd547913"],["/polyfills.20d568069cc37df12f96.bundle.js","428888c17e3f91cca163fdfe699abdf9"],["/styles.edc0e8c37803bf2ee9f0.bundle.css","edc0e8c37803bf2ee9f071cad7cdda3c"]];
+var precacheConfig = [["/index.html","05912e32a0a57b934f8b61b5225b32be"],["/inline.49af03f940af0fd47078.bundle.js","91574ef453d27fb8b205b4defd16ed37"],["/main.95e9f4bf97204f08e84e.bundle.js","7361e874b6671838983969d8fd547913"],["/polyfills.20d568069cc37df12f96.bundle.js","428888c17e3f91cca163fdfe699abdf9"],["/styles.edc0e8c37803bf2ee9f0.bundle.css","edc0e8c37803bf2ee9f071cad7cdda3c"]];
 var cacheName = 'sw-precache-v3-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
